9ab1a994c1b2148b0436b14d7e55e25ed05d69da
jeromechavez <jayrome12@yahoo.com>
2018-07-24 14:34:17 -0700

    Removed any Bootstrap classes. #2

--- client/components/part-filter.js
+++ client/components/part-filter.js
@@ -1,80 +1,48 @@
 import React from 'react'
-import Link from './link'
+import AppBar from '@material-ui/core/AppBar'
+import Tabs from '@material-ui/core/Tabs'
+import Tab from '@material-ui/core/Tab'
 
-export default function PartFilter({ activeType }) {
-  return (
-    <div className="row mx-3">
-      <ul className="nav flex-column nav-pills list-filters">
-        <li className="nav-item">
-          <Link
-            path="parts"
-            isActive={activeType === ''}> All </Link>
-        </li>
-        <li className="nav-item">
-          <Link
-            path="parts"
-            params={{ type: 'processor' }}
-            isActive={activeType === 'processor'}>
-            CPU Processors
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link 
-            path="parts"
-            params={{ type: 'cooling' }}
-            isActive={activeType === 'cooling'}>
-            CPU Coolers
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link
-            path="parts"
-            params={{ type: 'motherboard' }}
-            isActive={activeType === 'motherboard'}>
-            Motherboards
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link
-            path="parts"
-            params={{ type: 'case' }}
-            isActive={activeType === 'case'}>
-            Cases
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link
-            path="parts"
-            params={{ type: 'memory' }}
-            isActive={activeType === 'memory'}>
-            Memory(RAM)
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link
-            path="parts"
-            params={{ type: 'GPU' }}
-            isActive={activeType === 'GPU'}>
-            Video Cards
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link
-            path="parts"
-            params={{ type: 'storage' }}
-            isActive={activeType === 'storage'}>
-            Storage
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link
-            path="parts"
-            params={{ type: 'powersupply' }}
-            isActive={activeType === 'powersupply'}>
-            Power Supplies
-          </Link>
-        </li>
-      </ul>
-    </div>
-  )
+const styles = {
+  root: {
+    backgroundColor: 'black'
+  }
+}
+export default class PartFilter extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleChange = this.handleChange.bind(this)
+    this.state = {
+      value: 0
+    }
+  }
+
+  handleChange(event, value) {
+    this.setState({ value })
+  }
+  render() {
+    return (
+      <div>
+        <AppBar position="static" style={styles.root}>
+          <Tabs 
+            value={this.state.value} 
+            onChange={this.handleChange} 
+            indicatorColor="primary"
+            textColor="inherit"
+            scrollable
+          >
+            <Tab label="All" href="#parts"/>
+            <Tab label="Processors" href="#parts?type=processor" />
+            <Tab label="Cooling" href="#parts?type=cooling"/>
+            <Tab label="Motherboards" href="#parts?type=motherboard"/>
+            <Tab label="Video Cards" href="#parts?type=GPU" />
+            <Tab label="Cases" href="#parts?type=case"/>
+            <Tab label="Memory(RAM)" href="#parts?type=memory"/>
+            <Tab label="Storage" href="#parts?type=storage"/>
+            <Tab label="Power Supplies" href="#parts?type=powersupply"/>
+          </Tabs>
+        </AppBar>
+      </div>
+    )
+  }
 }
