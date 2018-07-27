70aae8841a42e43d274c351f4fca4711667676d2
jeromechavez <jayrome12@yahoo.com>
2018-07-27 10:49:52 -0700

    Defined a function component that renders a table of the computer parts currently selected by the user with current price total. #4

--- client/components/build-buttons.js
+++ client/components/build-buttons.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import Button from '@material-ui/core/Button'
 
+const styles = {
+  current: {
+    margin: 'auto 10px'
+  }
+}
+
 export default function BuildButtons({ activeStep, onBack, onNext, disabled, steps, showBuild }) {
   const buttonLabel = (activeStep === steps.length - 1) ? 'Finish' : 'Next'
   return (
@@ -9,7 +15,7 @@ export default function BuildButtons({ activeStep, onBack, onNext, disabled, ste
       <Button variant="contained" color="primary" disabled={ disabled } onClick={ onNext }>
         { buttonLabel }
       </Button>
-      <Button variant="contained" color="primary" onClick={ showBuild }>Current Build</Button>
+      <Button variant="contained" style={styles.current} color="primary" onClick={ showBuild }>Current Build</Button>
     </div>
   )
 }
