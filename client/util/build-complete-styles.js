684f9872eacfac6f71e778b9219dca065995b88b
jeromechavez <jayrome12@yahoo.com>
2018-07-30 11:10:27 -0700

    Defined all the styles and positioning of build complete view into one file #13

---
+++ client/util/build-complete-styles.js
@@ -0,0 +1,52 @@
+module.exports = {
+  paper: {
+    width: '75vw',
+    height: '800px',
+    margin: '10vh auto auto',
+    backgroundImage: `url('./images/view-build-background.png')`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    position: 'relative'
+  },
+  processor: {
+    position: 'absolute',
+    top: '250px',
+    left: '310px'
+  },
+  motherboard: {
+    position: 'absolute',
+    left: '30%',
+    bottom: '30%'
+  },
+  memory: {
+    position: 'absolute',
+    top: '250px',
+    left: '400px'
+  },
+  videocard: {
+    position: 'absolute',
+    top: '400px',
+    left: '100px'
+  },
+  case: {
+    position: 'absolute',
+    left: '40%',
+    top: '10px'
+  },
+  powersupply: {
+    position: 'absolute',
+    bottom: '10px',
+    left: '100px'
+  },
+  cooling: {
+    position: 'absolute',
+    top: '350px',
+    right: '150px'
+  },
+  storage: {
+    position: 'absolute',
+    bottom: '50px',
+    right: '200px'
+  }
+}
