903fce8ccbfbba093baca52ff27391e45b910786
jeromechavez <jayrome12@yahoo.com>
2018-08-02 11:35:20 -0700

    Fixed component call from material-ui #20

--- client/components/nav-bar.js
+++ client/components/nav-bar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Button from '@material-ui/core/Button'
-import ToolBar from '@material-ui/core/ToolBar'
+import Toolbar from '@material-ui/core/Toolbar'
 
 const styles = {
   root: {
@@ -16,12 +16,12 @@ const styles = {
 export default function NavBar(props) {
   return (
       <AppBar position="static" style={ styles.root }>
-        <ToolBar>
+        <Toolbar>
           <Button color="inherit" href="#home"><img src="./images/app-logo.png" style={styles.logo} /></Button>
           <Button color="inherit" href="#parts">Parts</Button>
           <Button color="inherit" href="#build">Build A Rig</Button>
           <Button color="inherit" href="#buildcomplete">View Build</Button>
-        </ToolBar>
+        </Toolbar>
       </AppBar>
   )
 }
