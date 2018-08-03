328b984ed43907bf7cf847d3e434f8fcf214b380
jeromechavez <jayrome12@yahoo.com>
2018-08-03 12:57:53 -0700

    Refactored button to handle both save and update features  #36

--- client/components/build-save.js
+++ client/components/build-save.js
@@ -1,47 +1,46 @@
 import React from 'react'
-import Typography from '@material-ui/core/Typography'
 import TextField from '@material-ui/core/TextField'
 import Grid from '@material-ui/core/Grid'
 import Button from '@material-ui/core/Button'
 
 const styles = {
   input: {
-    width: '640px'
+    width: '80%'
+  },
+  root: {
+    marginTop: '10px'
   },
   grid: {
-    width: '830px',
+    width: '400px',
     marginLeft: '10px'
   },
   gridInput: {
     height: '75px',
-    width: '650px'
-  },
-  root: {
-    marginTop: '20px'
+    width: '80%'
   },
   header: {
     marginLeft: '20px'
   },
   banner: {
-    width: '750px',
+    width: '400px',
     height: '50px',
-    backgroundImage: `url('./images/save-your-rig.png')`,
+    backgroundImage: `url('./images/save-rig.png')`,
     backgroundRepeat: 'no-repeat'
   }
 }
 
-export default function BuildSave({ change, submit, saved, name, updated, overwrite }) {
+export default function BuildSave({ change, submit, saved, name, buildId }) {
+  const action = (buildId) ? 'Update' : 'Save'
   const defaultValue = (!name) ? '' : name
   return (
     <div style={styles.root}>
       <div style={styles.banner}></div>
       <Grid container wrap="wrap" alignItems="flex-end" spacing={8} style={styles.grid}>
-        <Grid item xs={11} style={styles.gridInput}>
+        <Grid item xs={9} style={styles.gridInput}>
           <TextField id="name" label="Name" margin="normal" style={styles.input} value={defaultValue} onChange={change}></TextField>
         </Grid>
         <Grid item xs={1}>
-          <Button id="button" variant="contained" disabled={updated} onClick={overwrite}>Update</Button>
-          <Button id="button" variant="contained" disabled={saved} onClick={submit}>Save</Button>
+          <Button id="button" variant="contained" disabled={saved} onClick={submit}>{action}</Button>
         </Grid>
       </Grid>
     </div>
--- client/views/build-complete-view.js
+++ client/views/build-complete-view.js
@@ -1,20 +1,10 @@
 import React from 'react'
-import BuildMap from '../components/build-map'
-
-const styles = {
-  banner: {
-    width: '750px',
-    height: '50px',
-    backgroundRepeat: 'no-repeat',
-    backgroundImage: `url('./images/your-rig.png')`
-  }
-}
+import BuildComplete from '../components/build-complete'
 
 export default function BuildCompleteView({ parts }) {
   return (
     <div>
-      <div style={styles.banner}></div>
-      <BuildMap parts={parts} />
+      <BuildComplete parts={parts} />
     </div>
   )
 }
