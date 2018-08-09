6fa02298531bc3d2690959e71ecf3576f400016b
jeromechavez <jayrome12@yahoo.com>
2018-08-09 00:12:49 -0700

    Fixed a spacing issue for when the build wizard completes

--- client/components/reset-button.js
+++ client/components/reset-button.js
@@ -2,11 +2,17 @@ import React from 'react'
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 
+const styles = {
+  header: {
+    marginLeft: '10px'
+  }
+}
+
 
 export default function ResetButton({ onClick }) {
   return (
     <div>
-      <Typography>All steps completed - you&quot;re finished!</Typography>
+      <Typography style={styles.header}>All steps completed - you&quot;re finished!</Typography>
       <Button onClick={ onClick }>Reset</Button>
     </div>
   )
